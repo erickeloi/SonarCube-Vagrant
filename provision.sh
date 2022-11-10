9a6e31c3827084d14ad17185696e593da0e11d9b
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2022-11-10 17:19:02 -0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,8 +1,12 @@
 #/usr/bin/bash
-useradd sonar
-sudo yum install wget unzip java-11-openjdk-devel -y
+
 sudo su
+
+useradd sonar
+yum install epel-release -y
+yum install wget unzip java-11-openjdk-devel -y
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.7.0.61563.zip
+
 unzip sonarqube-9.7.0.61563.zip -d /opt/
 mv /opt/sonarqube-9.7.0.61563 /opt/sonarqube
 chown -R sonar:sonar /opt/sonarqube 
@@ -23,14 +27,16 @@ Restart=always
 WantedBy=multi-user.target
 EOT
 service sonar start
+service sonar enable
 
 # instalar sonar scanner
-sudo wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.7.0.2747-linux.zip
-sudo unzip sonar-scanner-cli-4.7.0.2747-linux.zip -d /opt/
-sudo mv /opt/sonar-scanner-cli-4.7.0.2747-linux /opt/sonar-scanner
-sudo chown -R sonar:sonar /opt/sonar-scanner
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.7.0.2747-linux.zip
+unzip sonar-scanner-cli-4.7.0.2747-linux.zip -d /opt/
+mv /opt/sonar-scanner-4.7.0.2747-linux /opt/sonar-scanner
+chown -R sonar:sonar /opt/sonar-scanner
 echo 'export PATH=$PATH:/opt/sonar-scanner/bin' | sudo tee -a /etc/profile
-curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
-sudo yum install nodejs -y
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
+yum install nodejs -y
 
-
+systemctl enable sonar
+systemctl start sonar
